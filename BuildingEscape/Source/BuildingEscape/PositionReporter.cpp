f4df86c5d43785e91402591fec6e009378b82153
Bo Gardner <saaggs@hotmail.com>
2018-11-20 19:17:46 -0800

    "Added component files for the chair."

---
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2012660f9148cff0282a825ade092fb0a718d037f2204b70348412282800753e
+size 766
---
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39d51f8af1b3b50d73b081b4604e00a271969cd47cd3f9e37421d460d102ba17
+size 685
