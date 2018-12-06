59b69c97129da4023997051b08cbfe49306259fe
Bo Gardner <saaggs@hotmail.com>
2018-12-06 10:37:17 -0800

    "Objects report their X, Y, Z position instead of just their name"

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3baa902df0689147c7b91266c99dd4099727960222c3cbd5a06b04da90bbad
-size 951
+oid sha256:c4f3b24c2bda7f4bea1d720f1814b6488d9adadfff9b0c11b3218e5903c235b5
+size 1303
