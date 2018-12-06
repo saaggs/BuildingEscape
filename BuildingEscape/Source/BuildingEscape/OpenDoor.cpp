642013e907bfb0deab8ecd108a0ffd2776a67007
Bo Gardner <saaggs@hotmail.com>
2018-12-06 13:02:52 -0800

    "Added rotation to door so it opens outward 60Â"

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2742a8224c39f6b7fe630c85ebe210fbc5ab1b955c23eeaf4f4ad1e3cd059d1
+size 1876
---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68ab1cdb2b4e97d5741d13ae8c696310e8bd4901c3f2d96fba751cfd7340534a
+size 1264
