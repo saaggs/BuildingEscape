c897e67b809a1d628eac695b5cb92f94bbd30586
Bo Gardner <saaggs@hotmail.com>
2018-12-08 00:12:46 -0800

    "Added a trigger volume to the floor and connected to the door."

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68ab1cdb2b4e97d5741d13ae8c696310e8bd4901c3f2d96fba751cfd7340534a
-size 1264
+oid sha256:08858e1b2916655bb9046db34f1847027a9d248966593db7cf123be218574f9d
+size 1580
