d0382ea4554bdf44d6b132bfc0957532d72da5dc
DESKTOP-DGNS3IU\Bo Jango <saaggs@hotmail.com>
2018-12-11 20:33:17 -0800

    Set Default Pawn to ActorThatOpens

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b40d261ca0674f1b3f7ed1b156161fc010923160d19ef02e1da13f9806261cd
-size 2288
+oid sha256:1cc13cf9e8823f79f2ca85646b59d5c7bab4ced5ea6bc097721b2198d88a9cfc
+size 2528
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acc73f1c7c47b69cbc53df4f0e1d33c2318c34a63b286ccccbc8a5512fb73d22
-size 2038
+oid sha256:2cafb44759aed87982da0f4c932b98d6b348269afadd677cf0edc490c99e801b
+size 2174
