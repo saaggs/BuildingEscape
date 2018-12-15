4eaea33987f283117826c0a09cbc596c690d3ae1
DESKTOP-DGNS3IU\Bo Jango <saaggs@hotmail.com>
2018-12-15 14:45:42 -0800

    Refactored grabber, created method to discover first objec hit with Line-trace

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78b109b2043ba9bfc75f0af7ae598d4e204a7bea347d9dfec3096d3c87b706a2
-size 6152
+oid sha256:55c410df210d4dd1bf17393154e287b67c035b90a63699d5dd72d3ff42c0f276
+size 7594
