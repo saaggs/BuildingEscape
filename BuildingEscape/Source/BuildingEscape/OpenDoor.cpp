549b06eb30cf1dc10e98312e7710061228209914
DESKTOP-DGNS3IU\Bo Jango <saaggs@hotmail.com>
2018-12-16 15:01:47 -0800

    Door swings close via assigned blueprint

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:588e79fdba3f6f4cf16db34dfa7d8e1a8fa8a0fc9e5d64826d0ca162a1531c08
-size 4442
+oid sha256:dff9d2f6157127ad4b8642b0f24b0156fccf46fe97abeb47597d890a8119b9d3
+size 3614
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea1872bb59929d742f4262abd0a46cf6f4d75697922689364476ba0ae84c9058
-size 2844
+oid sha256:e864de0ab6c7868e8a2f95a9e9139823c91ec2f19a79d08ede871dfc0b2df03a
+size 2304
