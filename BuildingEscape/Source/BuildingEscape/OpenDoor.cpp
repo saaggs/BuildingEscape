ddccae2c622de2fd1e413fc001596e2f307cbde5
DESKTOP-DGNS3IU\Bo Jango <saaggs@hotmail.com>
2018-12-16 13:44:54 -0800

    Added Blueprint assigned door for open door

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:254fbee000a685fc712b13421418d74d2d998718358cb339544567c0e00561ee
-size 4380
+oid sha256:588e79fdba3f6f4cf16db34dfa7d8e1a8fa8a0fc9e5d64826d0ca162a1531c08
+size 4442
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86da3961949bd0b79efdab924e028932f4b9809c3f62a86e711fdb103c45b23c
-size 2602
+oid sha256:ea1872bb59929d742f4262abd0a46cf6f4d75697922689364476ba0ae84c9058
+size 2844
